9fb5234df326d5444ab3be700e1fecff5f29abe5
Quinton Williams <quinw68@gmail.com>
2013-11-07 23:24:17 -0700

    First commit
    First commit

---
+++ QRAG/Scripts/Core/Actors/Character.js
@@ -0,0 +1,128 @@
+﻿/// <reference path="Actor.js" />
+/// <reference path="../IO/Keyboard.js" />
+/// <reference path="../three_js" />
+
+Character =  function()
+{
+    Actor.apply(this, arguments);
+    scope = this;
+    this.keyboard = IO.keyboard;
+    this.rotation = 0;
+    IO.controls.noKeys = true;
+    this.forward = false;
+    this.backward = false;
+    this.left = false;
+    this.right = false;
+    this.sidestepleft = false;
+    this.sidestepright = false;
+    this.height = 5;
+    IO.controls.target = this;
+    IO.controls.maxDistance = 30;
+    
+    increment = function ()
+    {
+
+    }
+    keydown = function (event)
+    {
+        switch(event.keyCode){
+            case Keyboard.KEYS.w:
+                scope.forward = true;
+                break;
+            case Keyboard.KEYS.s:
+                scope.backward = true;
+                break;
+            case Keyboard.KEYS.a:
+                scope.left = true;
+                break;
+            case Keyboard.KEYS.d:
+                scope.right = true;
+                break;
+            case Keyboard.KEYS.q:
+                scope.sidestepleft = true;
+                break;
+            case Keyboard.KEYS.e:
+                scope.sidestepright = true;
+        }
+    }
+
+    keyup = function (event)
+    {
+        switch (event.keyCode)
+        {
+            case Keyboard.KEYS.w:
+                scope.forward = false;
+                break;
+            case Keyboard.KEYS.s:
+                scope.backward = false;
+                break;
+            case Keyboard.KEYS.a:
+                scope.left = false;
+                break;
+            case Keyboard.KEYS.d:
+                scope.right = false;
+                break;
+            case Keyboard.KEYS.q:
+                scope.sidestepleft = false;
+                break;
+            case Keyboard.KEYS.e:
+                scope.sidestepright = false;
+        }
+    }
+
+    // Slowly move the camera to behind the target
+    this.updateCamera = function () {
+        var diff = (scope.rotation - (IO.controls.theta + Math.PI)%(2*Math.PI)) % (2* Math.PI);
+        if (Math.abs(diff) <= Math.PI / 20) {
+            IO.controls.theta = (scope.rotation + Math.PI) % (2 * Math.PI);
+            return;
+        }
+        if (diff >= 0) {
+            IO.controls.theta += (Math.PI / 50) % (2*Math.PI);
+        } else {
+            IO.controls.theta -= (Math.PI / 50) % (2*Math.PI);
+        }
+    }
+
+    this.keyboard.addListener(this.keyboard.EVENTS.KEYDOWN, keydown);
+    this.keyboard.addListener(this.keyboard.EVENTS.KEYUP, keyup);
+};
+
+Character.prototype = new Actor();
+
+Character.prototype.update = function ()
+{
+        
+    if (scope.forward)
+    {
+        scope.translateZ(1);
+        scope.updateCamera();
+    }
+    if (scope.right)
+    {
+        scope.rotation += -Math.PI / 100 % (2 * Math.PI);
+        this.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), scope.rotation);
+        scope.updateCamera();
+    }
+    if (scope.left)
+    {
+        scope.rotation += Math.PI / 100 % (2 * Math.PI);
+        this.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), scope.rotation);
+        scope.updateCamera();
+    }
+    if (scope.sidestepleft)
+    {
+        scope.translateX(1);
+        RENDERER.camera.translateX(-1);
+    }
+    if (scope.sidestepright)
+    {
+        scope.translateX(-1);
+        RENDERER.camera.translateX(1);
+    }
+    if (scope.backward)
+    {
+        scope.translateZ(-1);
+        scope.updateCamera();
+    }
+}
---
+++ QRAG/Scripts/Core/main.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="three_js/three.min.js" />
+/// <reference path="../jquery-1.8.2.js" />
+/// <reference path="Terrain/Terrain.js" />
+/// <reference path="Renderer/Renderer.js" />
+/// <reference path="Actors/Character.js" />
+/// <reference path="Actors/Actor.js" />
+
+$(document).ready(function () {
+    initGL();
+    mainLoop();
+});
+
+// GLOBALS
+var CORE_PATH = "Scripts/Core/";
+var RENDERER;
+var TERRAIN;
+var IO;
+var IDCOUNTER = 0;
+var ACTORS = [];
+
+function initGL() {
+    RENDERER = new Renderer();
+    TERRAIN = new Terrain();
+    IO = new Input();
+    $('#scene').append(RENDERER.renderer.domElement);
+    
+    TERRAIN.InitTerrain();
+    MainChar = new Character();
+    ACTORS.push(MainChar);
+}
+
+function mainLoop() {
+    requestAnimationFrame(mainLoop);
+    RENDERER.render();
+}
+
