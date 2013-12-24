04235760b1654347270b3923fa9b51c3c694cfef
quinw68 <quinw68@gmail.com>
2013-12-23 17:51:28 -0700

    Camera changes

--- client/Core/Actors/Character.js
+++ client/Core/Actors/Character.js
@@ -72,15 +72,15 @@ Character =  function()
         var target = (scope.rotation + Math.PI) % (2 * Math.PI);
         
         var diff = target - IO.controls.theta;
-        console.log("diff: " + diff);
-        if (diff < 0) {
-            diff = 2 * Math.PI - diff;
-        }
-        var rotation = 0;
+        console.log(diff);
         if (Math.abs(diff) <= Math.PI / 20) {
             IO.controls.theta = target;
             return;
         }
+        if (diff < 0) {
+            diff = 2 * Math.PI + diff;
+        }
+        var rotation = 0;
         if ((diff > 0 && diff < Math.PI))
         {
             rotation = (IO.controls.theta + Math.PI / 50);
@@ -89,25 +89,9 @@ Character =  function()
             rotation = (IO.controls.theta - Math.PI / 50);
         }
         if (rotation < 0) {
-            rotation = 2 * Math.PI - rotation;
+            rotation = 2 * Math.PI + rotation;
         }
         IO.controls.theta = rotation;
-        //var diff2 = (scope.rotation + 2 * Math.PI) - IO.controls.theta;
-        //var min = diff1;
-        //if(Math.abs(diff2) < Math.abs(diff1)){
-        //    min = diff2;
-        //}
-        ////var diff = (scope.rotation - (IO.controls.theta + Math.PI)%(2*Math.PI)) % (2* Math.PI);
-        //var diff = (min - Math.PI) % (2*Math.PI);
-        //if (Math.abs(diff) <= Math.PI / 20) {
-        //    IO.controls.theta = (scope.rotation + Math.PI) % (2 * Math.PI);
-        //    return;
-        //}
-        //if (diff >= 0) {
-        //    IO.controls.theta += (Math.PI / 50) % (2*Math.PI);
-        //} else {
-        //    IO.controls.theta -= (Math.PI / 50) % (2*Math.PI);
-        //}
     }
 
     this.keyboard.addListener(this.keyboard.EVENTS.KEYDOWN, keydown);
@@ -127,13 +111,11 @@ Character.prototype.update = function ()
     }
     if (scope.right)
     {
-        console.log("Before: " + scope.rotation);
         scope.rotation = (scope.rotation - Math.PI / 100);
         if (scope.rotation < 0) {
             scope.rotation = 2 * Math.PI - scope.rotation;
         }
         this.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), scope.rotation);
-        console.log(scope.rotation + ' - ' + IO.controls.theta);
         scope.updateCamera();
     }
     if (scope.left)
@@ -143,17 +125,18 @@ Character.prototype.update = function ()
             scope.rotation = scope.rotation % (2 * Math.PI);
         }
         this.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), scope.rotation);
-        console.log(scope.rotation + ' - ' + IO.controls.theta);
         scope.updateCamera();
     }
     if (scope.sidestepleft)
     {
         scope.translateX(1);
+        scope.position.y = TERRAIN.GetHeight(scope.position.x, scope.position.z);
         RENDERER.camera.translateX(-1);
     }
     if (scope.sidestepright)
     {
         scope.translateX(-1);
+        scope.position.y = TERRAIN.GetHeight(scope.position.x, scope.position.z);
         RENDERER.camera.translateX(1);
     }
     if (scope.backward)
